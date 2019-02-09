7760593c1c3e27806eff3861457d40d1a406d13d
Marcus Stollsteimer <sto.mar@web.de>
2019-02-09 21:26:27 +0100

    Use expand_path with __dir__

--- test/helper.rb
+++ test/helper.rb
@@ -8,9 +8,9 @@ require_relative "../app"
 
 
 def setup_database
-  load File.expand_path("../../db/schema.rb", __FILE__)
-  load File.expand_path("../../db/seeds.rb", __FILE__)
-  load File.expand_path("../fixtures.rb", __FILE__)
+  load File.expand_path("../db/schema.rb", __dir__)
+  load File.expand_path("../db/seeds.rb", __dir__)
+  load File.expand_path("fixtures.rb", __dir__)
 end
 
 def teardown_database
