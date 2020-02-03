0edbcb4b8960c39cbbbb4e91989589d34da36306
Marcus Stollsteimer <sto.mar@web.de>
2020-02-03 20:12:29 +0100

    Remove redundant spaces

--- db/schema.rb
+++ db/schema.rb
@@ -3,7 +3,7 @@
 require_relative "connection"
 
 DB.create_table?(:items) do
-  primary_key  :id
-  String       :name
-  Time         :created_at
+  primary_key :id
+  String :name
+  Time :created_at
 end
