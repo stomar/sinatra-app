173b7767a20a9f16137f5331376d512c66252907
Marcus Stollsteimer <sto.mar@web.de>
2020-02-03 20:29:07 +0100

    Check for database tables before creating schema
    When a table already exists, create_table? silently does nothing.
    So after changing schema.rb and running 'rake db:create' the database
    might still have the outdated schema and not the expected one.
    
    Instead, output a warning and exit when tables already exist, providing
    immediate feedback that the database might not be in the expected state.

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,12 @@
 
 require_relative "connection"
 
-DB.create_table?(:items) do
+unless DB.tables.empty?
+  warn "Database does already have tables. Not loading schema."
+  exit 1
+end
+
+DB.create_table(:items) do
   primary_key :id
   String :name
   Time :created_at
